ad4222adac5a1970c532bd90cd68754cc3f5fa7a
Marko Kosunen <marko.kosunen@aalto.fi>
2021-10-03 09:42:51 +0300

    Cleanup init_module

--- shell/init_module.sh
+++ shell/init_module.sh
@@ -225,27 +225,7 @@ cat <<EOF > init_submodules.sh
 #!/usr/bin/env bash
 #Init submodules in this dir, if any
 DIR="\$( cd "\$( dirname \$0 )" && pwd )"
-git submodule update --init
-
-#Publish local the ones you need
-#cd \$DIR/clkdiv_n_2_4_8
-#sbt publishLocal
-
-#Selectively, init submodules of a larger projects
-#cd \$DIR/rocket-chip
-#git submodule update --init chisel3
-#git submodule update --init firrtl
-#git submodule update --init hardfloat
-
-#Recursively update submodeles
-#cd \$DIR/eagle_serdes
-#git submodule update --init --recursive serdes_top
-#sbt publishLocal
-#Assemble executables
-###sbt publishing
-#cd \$DIR/rocket-chip/firrtl
-#sbt publishLocal
-#sbt assembly
+git submodule sync
 
 #Recursively init submodules
 #SUBMODULES="\\
@@ -253,6 +233,7 @@ git submodule update --init
 #    f2_cm_serdes_lane \\
 #    "
 #for module in \$SUBMODULES; do
+#    git submodule update --init \${module}
 #    cd \${DIR}/\${module}
 #    if [ -f "./init_submodules.sh" ]; then
 #        ./init_submodules.sh
